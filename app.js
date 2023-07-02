96e1f13313a3aed04c5600f2c4bee4d552c8e65c
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-07-02 00:20:39 -0700

    finished express server security

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const csurf = require('csurf');
+const {isProduction} = require('./config/keys');
+
 const usersRouter = require('./routes/api/users'); // update the import file path
 const tweetsRouter = require('./routes/api/tweets'); // update the import file path
-
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -15,8 +19,29 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express
+    // server will serve the React files statically.)
+    app.use(cors());
+}
+
+// ...
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
 // Attach Express routers
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/tweets', tweetsRouter); // update the path
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
